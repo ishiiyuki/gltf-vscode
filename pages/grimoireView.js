6edf0a8889dcf861d07395113305e23c2f9cda1b
m2wasabi <m2wasabi@gmail.com>
2017-12-14 17:05:44 +0900

    Preview script on Grimoire.js

--- pages/grimoireView.js
+++ pages/grimoireView.js
@@ -2,10 +2,15 @@ var GrimoireView = function() {
     var enabled = false;
 
     this.cleanup = function() {
+        gr("*")("goml").remove();
     };
 
     this.startPreview = function() {
+        mainViewModel.hasBackground(false);
+        gr.Node.GomlLoader.loadForPage();
+        gr.Node.GomlLoader.loadFromScriptTag();
         var rootPath = document.getElementById("gltfRootPath").textContent;
-        var gltfContent = document.getElementById('gltf').textContent;
+        var fileName = document.getElementById('gltfFileName').textContent;
+        gr("*")("scene").append('<model src="' + rootPath + fileName + '" />');
     };
 };
